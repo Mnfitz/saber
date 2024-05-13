f8f59ee160dbd48f890489112231650bc38a8c92
j3fitz <32435231+j3fitz@users.noreply.github.com>
2024-05-13 08:34:30 -0700

    Fix usage of source_groups cmake directive, add /MP (multi-processor) compile support

--- cmake/cxxflags_common.cmake
+++ cmake/cxxflags_common.cmake
@@ -14,18 +14,46 @@ endmacro()
 
 # Fixup default compiler settings
 if (MSVC)
-    add_compile_options(
+	# Addn'l -D #defines to make #include<windows.h> less painful
+	add_definitions(-DNOMINMAX=1 -DWIN32_LEAN_AND_MEAN=1)
+	add_compile_options(
         # Be as strict as reasonably possible, since we want to support consumers using strict warning levels
-        /W4 /WX
-        )
-else()
+        /W4 /WX)
+
+	# For some unknown reason, 'RelWithDebInfo' compiles with '/Ob1' as opposed to '/Ob2' which prevents inlining of
+	# functions not marked 'inline'. The reason we prefer 'RelWithDebInfo' over 'Release' is to get debug info, so manually
+	# revert to the desired (and default) inlining behavior as that exercises more interesting code paths
+	if ("${CMAKE_BUILD_TYPE}" STREQUAL "RelWithDebInfo")
+		# TODO: This is currently blocked by an apparent Clang bug: https://github.com/llvm/llvm-project/issues/59690
+		# replace_cxx_flag("/Ob1" "/Ob2")
+		replace_cxx_flag("/Ob1" "/Ob2")
+	endif() # STREQUAL "RelWithDebInfo"
+
+	# Turn on "multi-procssor compilation" switch
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+
+else() # !MSVC
     # Clang with non-MSVC commandline syntax
     add_compile_options(
         # Effectively the same as /W4 /WX
         -Wall -Werror
     )
-endif()
+endif() # !MSVC
 
+# Xcode: Enable some extra debug-facilities for debug builds...
+if(XCODE)
+	if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+		# Generate XCode schema files
+		set(CMAKE_XCODE_GENERATE_SCHEME TRUE)
+		# Make malloc write 0xAA to newly allocated memory and 0x55 to deallocated memory
+		set(CMAKE_XCODE_SCHEME_MALLOC_SCRIBBLE YES)
+		# Place guard pages on each side of large (4096 bytes or more) buffers
+		set(CMAKE_XCODE_SCHEME_MALLOC_GUARD_EDGES YES)
+	endif() # STREQUAL "Debug"
+
+endif() # XCODE
+
+# Clang: Turn off misc warnings...
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     add_compile_options(
         # Ignore some pedantic warnings enabled by '-Wextra'
