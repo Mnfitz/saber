a9d8f57ddda3d2546acf86f000add5d038714cb6
Mnfitz <mn-fitz@hotmail.com>
2024-06-15 13:23:14 -0700

    SABR-15: "Create `Point` class for saber geometry" (#12)
    * Point class and tests + fixed cmakelist issue
    
    * Initial checkin of the Point class
    
    * Made move ctor noexcept
    
    ---------
    
    Co-authored-by: mnfitz-ASU <mnfitzg1@asu.edu>

---
+++ test/geometry_unittest.cpp
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2024 Matthew Fitzgerald
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+/////////////////////////////////////////////////////////////////////
+
+// catch2
+#include "catch2/catch_test_macros.hpp"
+
+// saber
+#include "saber/geometry/point.hpp"
+
+// std
+#include <cstdio>
+#include <type_traits>
+
+TEST_CASE("saber::geometry::point", "[saber]")
+{
+	SECTION("Default Construct")
+	{
+		saber::geometry::Point<int> point{};
+		REQUIRE(point.X() == 0);
+		REQUIRE(point.Y() == 0);
+	}
+
+	SECTION("Alt Construct")
+	{
+		saber::geometry::Point<int> point{1, 2};
+		REQUIRE(point.X() == 1);
+		REQUIRE(point.Y() == 2);
+	}
+
+    SECTION("Copy Construct")
+	{
+		saber::geometry::Point<int> point1{1,2};
+        saber::geometry::Point<int> point2{point1};
+		REQUIRE(point2.X() == 1);
+		REQUIRE(point2.Y() == 2);
+	}
+
+    SECTION("Copy Assign")
+	{
+		saber::geometry::Point<int> point1{1,2};
+        saber::geometry::Point<int> point2 = point1;
+		REQUIRE(point2.X() == 1);
+		REQUIRE(point2.Y() == 2);
+	}
+
+    SECTION("Move Construct")
+	{
+		saber::geometry::Point<int> point1{1,2};
+        saber::geometry::Point<int> point2{std::move(point1)};
+		REQUIRE(point2.X() == 1);
+		REQUIRE(point2.Y() == 2);
+	}
+
+    SECTION("Move Assign")
+	{
+		saber::geometry::Point<int> point1{1,2};
+        saber::geometry::Point<int> point2 = std::move(point1);
+		REQUIRE(point2.X() == 1);
+		REQUIRE(point2.Y() == 2);
+	}
+}
