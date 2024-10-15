f8a7578b2c38af1a0cc58503decfd080da7b1f0b
mnfitz-ASU <mnfitzg1@asu.edu>
2024-10-14 19:48:16 -0700

    Initial placeholders for SIMD implementation

---
+++ include/saber/geometry/detail/simd.hpp
@@ -0,0 +1,17 @@
+#ifndef SABER_GEOMETRY_DETAIL_SIMD_HPP
+#define SABER_GEOMETRY_DETAIL_SIMD_HPP
+
+namespace saber::geometry::detail {
+
+template<typename T, int NBits>
+class Simd
+{
+public:
+    
+private:
+
+}; // struct Simd<>
+
+} // namespace saber::geometry::detail
+
+#endif // SABER_GEOMETRY_DETAIL_SIMD_HPP
