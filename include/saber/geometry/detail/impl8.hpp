b5124ce1eb46fd8dcdad1d9d9a814c8926ce12ee
mnfitz-ASU <mnfitzg1@asu.edu>
2025-12-22 11:44:11 -0800

    Added skeletons of impl8 and matrix classes

---
+++ include/saber/geometry/detail/impl8.hpp
@@ -0,0 +1,180 @@
+#ifndef SABER_GEOMETRY_DETAIL_IMPL8_HPP
+#define SABER_GEOMETRY_DETAIL_IMPL8_HPP
+
+// std
+#include <array>
+#include <tuple>
+#include <type_traits>
+
+// saber
+#include "saber/inexact.hpp"
+#include "saber/geometry/detail/impl2.hpp"
+#include "saber/geometry/detail/Impl8.hpp"
+#include "saber/geometry/detail/simd.hpp"
+
+namespace saber::geometry::detail {
+
+// Helpers
+#pragma region
+
+template<typename T>
+constexpr bool Is32BitDataType() 
+{
+	constexpr bool kIs32Bit = (sizeof(T)*8 <= 32);
+	return kIs32Bit;
+}
+
+template<typename T>
+constexpr bool Is64BitDataType() 
+{
+	constexpr bool kIs64Bit = (sizeof(T)*8 == 64);
+	return kIs64Bit;
+}
+
+#pragma endregion
+
+template<typename T>
+struct Impl8 final
+{
+	class Simd;
+
+	class Scalar
+	{
+	public:
+		// default ctor
+		constexpr Scalar() = default;
+		~Scalar() = default;
+
+		// alt ctor
+		constexpr Scalar(T inFirst, T inSecond, T inThird, T inFourth, T inFifth, T inSixth, T inSeventh, T inEigth) :
+			mArray(inFirst, inSecond, inThird, inFourth, T inFifth, T inSixth, T inSeventh, T inEigth)
+		{
+			// Do nothing
+		}
+
+		constexpr Scalar(const typename Impl2<T>::Scalar& inFirst, const typename Impl2<T>::Scalar& inSecond, const typename Impl2<T>::Scalar& inThird, const typename Impl2<T>::Scalar& inFourth) :
+			mArray(inFirst.Get<0>(), inFirst.Get<1>(), inSecond.Get<0>(), inSecond.Get<1>(), inThird.Get<0>(), inThird.Get<1>(), inFourth.Get<0>(), inFourth.Get<1>())
+		{
+			// Do nothing
+		}
+
+		constexpr Scalar(const typename Impl4<T>::Scalar& inFirst, const typename Impl4<T>::Scalar& inSecond) :
+			mArray(inFirst.Get<0>(), inFirst.Get<1>(), inFirst.Get<2>(), inFirst.Get<3>(), inSecond.Get<0>(), inSecond.Get<1>(), inSecond.Get<2>(), inSecond.Get<3>())
+		{
+			// Do nothing
+		}
+
+		template<std::size_t Index>
+		constexpr const T& Get() const
+		{
+			static_assert(Index < std::size(mArray)>, "Provided index out of bounds.");
+			return mArray.at(Index);
+		}
+
+		template<std::size_t Index>
+		constexpr T& Get()
+		{
+			static_assert(Index < std::size(mArray)>, "Provided index out of bounds.");
+			return mArray.at(Index);
+		}
+
+		constexpr Scalar& operator+=(const Scalar& inRHS)
+		{
+			Get<0>() += inRHS.Get<0>();
+			Get<1>() += inRHS.Get<1>();
+			Get<2>() += inRHS.Get<2>();
+			Get<3>() += inRHS.Get<3>();
+			Get<4>() += inRHS.Get<4>();
+			Get<5>() += inRHS.Get<5>();
+			Get<6>() += inRHS.Get<6>();
+			Get<7>() += inRHS.Get<7>();
+			return *this;
+		}
+
+		constexpr Scalar& operator-=(const Scalar& inRHS)
+		{
+			Get<0>() -= inRHS.Get<0>();
+			Get<1>() -= inRHS.Get<1>();
+			Get<2>() -= inRHS.Get<2>();
+			Get<3>() -= inRHS.Get<3>();
+			Get<4>() -= inRHS.Get<4>();
+			Get<5>() -= inRHS.Get<5>();
+			Get<6>() -= inRHS.Get<6>();
+			Get<7>() -= inRHS.Get<7>();
+			return *this;
+			return *this;
+		}
+
+		constexpr Scalar& operator*=(const Scalar& inRHS)
+		{
+			// TODO: Proper matrix multiplication (DOT vs CROSS product?)
+			return *this;
+		}
+
+		constexpr Scalar& operator/=(const Scalar& inRHS)
+		{
+			// TODO: Proper matrix division
+			return *this;
+		}
+
+		constexpr bool IsEqual(const Scalar& inRHS) const
+		{
+			// Point said to copy/paste this section into the scalar portion
+			bool result = false;
+			if constexpr (std::is_floating_point_v<T>)
+			{   
+				// Floating point comparisons are always inexact within an epsilon
+				result = Inexact::IsEq(Get<0>(), inRHS.Get<0>()) 
+							&& Inexact::IsEq(Get<1>(), inRHS.Get<1>())
+							&& Inexact::IsEq(Get<2>(), inRHS.Get<2>())
+							&& Inexact::IsEq(Get<3>(), inRHS.Get<3>())
+							&& Inexact::IsEq(Get<4>(), inRHS.Get<4>())
+							&& Inexact::IsEq(Get<5>(), inRHS.Get<5>())
+							&& Inexact::IsEq(Get<6>(), inRHS.Get<6>())
+							&& Inexact::IsEq(Get<7>(), inRHS.Get<7>());
+			}
+			else
+			{
+				// Integer comparisons are always exact
+				result = (Get<0>() == inRHS.Get<0>()) 
+							&& (Get<1>() == inRHS.Get<1>())
+							&& (Get<2>() == inRHS.Get<2>())
+							&& (Get<3>() == inRHS.Get<3>())
+							&& (Get<4>() == inRHS.Get<4>())
+							&& (Get<5>() == inRHS.Get<5>())
+							&& (Get<6>() == inRHS.Get<6>())
+							&& (Get<7>() == inRHS.Get<7>());
+			}
+			return result;
+		}
+		
+	private:
+		//friend class Simd; // Permit Simd class to provide constexpr api
+
+	private:
+		std::array<T, 8> mArray; // Impl8: so 8 elements are assumed
+	}; // struct Scalar
+
+	// TODO mnfitz 22dec2025: Add SIMD implementation
+
+#pragma region struct Impl8Traits
+template<typename T, ImplKind Impl> // Primary template declaration
+struct Impl8Traits;
+
+template<typename T> // Partial template specialization
+struct Impl8Traits<T, ImplKind::kScalar>
+{
+	using ImplType = typename Impl8<T>::Scalar; // VOODOO: Nested template type requires `typename` prefix
+};
+
+template<typename T> // Partial template specialization
+struct Impl8Traits<T, ImplKind::kSimd>
+{
+	using ImplType = typename Impl8<T>::Simd; // VOODOO: Nested template type requires `typename` prefix
+};
+
+#pragma endregion
+
+} // namespace saber::geometry::detail
+
+#endif // SABER_GEOMETRY_DETAIL_IMPL8_HPP
---
+++ include/saber/geometry/matrix.hpp
@@ -0,0 +1,89 @@
+#ifndef SABER_GEOMETRY_MATRIX_HPP
+#define SABER_GEOMETRY_MATRIX_HPP
+#pragma once
+
+// saber
+#include "saber/geometry/config.hpp"
+#include "saber/geometry/operators.hpp"
+#include "saber/geometry/detail/impl8.hpp"
+#include "saber/utility.hpp"
+
+// std
+#include <utility>
+
+namespace saber::geometry {
+
+/// @brief Represents a 2x3 matrix in 2D space.
+/// @tparam T The type of the matrix's coordinates (e.g., float, double).
+/// @tparam ImplType The implementation kind (e.g., scalar or SIMD).
+template<typename T, ImplKind Impl = ImplKind::kDefault>
+class Matrix
+{
+public:
+	using ValueType = T;
+
+public:
+	/// @brief Default constructor. Initializes a matrix with default 0 values.
+	constexpr Matrix() = default;
+
+	constexpr Matrix(T inM11, T inM12, T inM13, T inM21, T inM22, T inM23);
+
+	/// @brief Destructor.
+	~Matrix() = default;
+
+	/// @brief Move constructor.
+	constexpr Matrix(Matrix&& ioMove) noexcept = default;
+
+	/// @brief Move assignment operator.
+	constexpr Matrix& operator=(Matrix&& ioMove) noexcept = default;
+
+	/// @brief Copy constructor.
+	constexpr Matrix(const Matrix& inCopy) = default;
+
+	/// @brief Copy assignment operator.
+	constexpr Matrix& operator=(const Matrix& inCopy) = default;
+
+private:
+	// Private APIs
+
+	/// @brief Checks if this matrix is equal to another.
+	/// @param inMatrix The matrix to compare with.
+	/// @return True if equal, false otherwise.
+	constexpr bool IsEqual(const Matrix& inMatrix) const;
+
+	// Friend functions
+private:
+	friend constexpr bool operator== <Matrix>(const Matrix& inLHS, const Matrix& inRHS);
+	friend constexpr bool operator!= <Matrix>(const Matrix& inLHS, const Matrix& inRHS);
+
+	template<typename T, ImplKind Impl>
+	friend constexpr bool IsEmpty(const Matrix<T, Impl>& inMatrix);
+
+private:
+	using ImplType = typename detail::Impl8Traits<T, Impl>::ImplType; // VOODOO: Nested template type requires `typename` prefix
+	ImplType mImpl{};
+}; // class Matrix<>
+
+// ------------------------------------------------------------------
+#pragma region Inline Class Functions
+
+// Ctors
+template<typename T, ImplKind Impl>
+inline constexpr Matrix<T, Impl>::Matrix(T inM11, T inM12, T inM21, T inM22, T inM31, T inM32) :
+	mImpl(inM11, inM12, inM21, inM22, inM31, inM32, 0, 0)
+{
+	// Do nothing
+}
+
+
+template<typename T, ImplKind Impl>
+inline constexpr bool Matrix<T, Impl>::IsEqual(const Matrix& inMatrix) const
+{
+	auto result = mImpl.IsEqual(inMatrix.mImpl);
+	return result;
+}
+
+// ------------------------------------------------------------------
+} // namespace saber::geometry
+
+#endif // SABER_GEOMETRY_MATRIX_HPP
