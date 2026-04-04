99e363bf912fabb67b4c0d46df9947c80ad83928
mnfitz-ASU <mnfitzg1@asu.edu>
2026-04-04 13:29:37 -0700

    skeleton of event notifier

---
+++ include/saber/event/events.hpp
@@ -0,0 +1,29 @@
+#ifndef SABER_EVENT_EVENTS_HPP
+#define SABER_EVENT_EVENTS_HPP
+
+// saber
+#include "saber/config.hpp"
+
+namespace saber::event {
+
+class EventDelegate
+{
+
+};
+
+class EventManager
+{
+	void Register()
+	{
+
+	}
+
+	void Unregister()
+	{
+
+	}
+};
+
+} // namespace saber::event
+
+#endif // SABER_EVENT_EVENTS_HPP
